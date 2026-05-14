651038d84e1e4d9616fb5b38ebae61c0468d6799
Michael Paras <mikeparas@gmail.com>
2026-05-14 08:55:51 -0700

    chore: setup actual enrich main app with RMQ
    * Update Docker build to compile TS and use proper start script
    * Add RMQ config to main.ts
    * Clean up tsconfig.json

--- services/enrich/src/main.ts
+++ services/enrich/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AsyncMicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { RMQTypeDeserializer } from './common/microservices/deserializers/rmq-type.deserializer';
+
+const buildRmqUrl = (configService: ConfigService) => {
+  const rmq_user = configService.get<string>('MQ_USER') ?? 'guest';
+  const rmq_password = encodeURIComponent(
+    configService.get<string>('MQ_PASSWORD') ?? 'guest',
+  );
+  const rmq_host = configService.get<string>('MQ_HOST') ?? 'localhost';
+  const rmq_port = configService.get<string>('MQ_PORT') ?? '5672';
+
+  const rmq_url = `amqp://${rmq_user}:${rmq_password}@${rmq_host}:${rmq_port}/`;
+  return rmq_url;
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<AsyncMicroserviceOptions>(
+    AppModule,
+    {
+      useFactory: (configService: ConfigService) => ({
+        transport: Transport.RMQ,
+        options: {
+          urls: [buildRmqUrl(configService)],
+          queue: configService.get<string>('MQ_QUEUE_EXTERNAL_DATA'),
+          deserializer: new RMQTypeDeserializer(),
+        },
+      }),
+      inject: [ConfigService],
+    },
+  );
+
+  await app.listen();
 }
-bootstrap();
+void bootstrap();
